6001ea30bf307221bbb19b275a2a1349eea10d6e
Oles <oles1stepanov@gmail.com>
2023-10-04 18:56:49 +0300

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+#include <QApplication>
+
+#include "mainwindow.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow app;
+    app.show();
+
+    return a.exec();
+}
